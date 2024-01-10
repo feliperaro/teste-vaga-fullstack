d26af7f8163acd6795f06b18262c08675c581007
FehRoque <feliperamosroque@gmail.com>
2024-01-10 13:28:00 -0300

    reading data.csv using fs and csv-parse

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,24 @@
-import express, { Express } from 'express';
+import express, { Express } from "express";
+import fs from "fs";
+import { parse } from "csv-parse";
 
 const app: Express = express();
 const port = 3000;
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/data", (_, res) => {
+  fs.readFile("../data.csv", function (err, fileData) {
+    if (err) throw err;
+    parse(fileData, { columns: false, trim: true }, function (err, rows) {
+      if (err) throw err;
+      console.log("rows.length", rows.length);
+    });
+  });
+
+  res.send("Hello Api data");
 });
 
 app.listen(port, () => {
